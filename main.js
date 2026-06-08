8bf7146d5465cb9a0489bb98ac75c9b714444601
silvavanderly-boop <silva.vanderly@escola.pr.gov.br>
2026-06-08 12:08:01 -0300

    b

---
+++ main.js
@@ -0,0 +1,22 @@
+const botoes = document.querySelectorAll(".botoes");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function (){
+        for (let j = 0; j <botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+ const contadores = document.querySelectorAll(".contador");
+ const tempoObjetivo1 = new Date("2027-01-20T00.00.00");
+ const tempoObjetivo2 = new Date("2027-09-14T00.00.00");
+ const tempoObjetivo3 = new Date("2027-01-01T00.00.00");
+ const tempoObjetivo4 = new Date("2027-08-15T00.00.00");
+
+ const tempo = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+    
