382de47825d23409b08a045ce44589943b290422
silvavanderly-boop <silva.vanderly@escola.pr.gov.br>
2026-06-15 11:31:41 -0300

    lçl

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const botoes = document.querySelectorAll(".botoes");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i = 0; i < botoes.length; i++){
-    botoes[i].onclick = function (){
-        for (let j = 0; j <botoes.length; j++) {
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -12,11 +12,10 @@ for(let i = 0; i < botoes.length; i++){
     }
 }
 
- const contadores = document.querySelectorAll(".contador");
- const tempoObjetivo1 = new Date("2027-01-20T00.00.00");
- const tempoObjetivo2 = new Date("2027-09-14T00.00.00");
- const tempoObjetivo3 = new Date("2027-01-01T00.00.00");
- const tempoObjetivo4 = new Date("2027-08-15T00.00.00");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2027-01-20T00:00:00");
+const tempoObjetivo2 = new Date("2027-09-14T00:00:00");
+const tempoObjetivo3 = new Date("2027-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2027-08-15T00:00:00");
 
- const tempo = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-    
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
